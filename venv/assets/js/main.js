7155f6a2014c40172e02799f0b3db39baed33e74
mariana.queiroz <mariana.queiroz@lais.huol.ufrn.br>
2021-07-18 22:36:36 -0300

    subindo completo

---
+++ venv/assets/js/main.js
@@ -0,0 +1,210 @@
+/**
+* Template Name: OnePage - v4.3.0
+* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+
+
+  /**
+   * Back to top button
+   */
+
+
+  /**
+   * Mobile nav toggle
+   */
+
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
---
+++ venv/assets/vendor/str.js
@@ -0,0 +1,106 @@
+$( document ).ready(function() {
+
+    function sleep(ms) {
+        return new Promise(
+            resolve => setTimeout(resolve, ms)
+        );
+    }
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var azul = '#000080';
+    var verm = '#800000';
+    var ama = '#DAA520';
+    var verde = '#006400';
+
+    function cores(){
+        var myArray = [azul, verde, ama, verm];
+        var rand = myArray[Math.floor(Math.random() * myArray.length)];
+        return rand;
+    }
+
+
+    async function acendeBotoes(){
+        var s = 2000;
+        var acerto = 0;
+        var total_esperado = 0;
+        var cor_ref = cores();
+        var inicio_tempo=0;
+        var fim_tempo =0;
+        var tempo_reacao = [];
+        $("#b18").css("background-color", cor_ref);
+        for (var i=1; i<30; i++){
+            var ran = getRandomIntInclusive(1, 16);
+            var id = "#b"+ran;
+            var clickButton = document.querySelector(id);
+            var cor = cores();
+            inicio_tempo = new Date().getTime();
+            $(id).css("background-color", cor);
+            await sleep(s);
+            $(id).keypress(function(){
+                console.log('okokok');
+                if (cor == cor_ref){
+                    acerto += 1;
+                    fim_tempo = new Date().getTime();
+                    console.log(fim_tempo-inicio_tempo);
+                    tempo_reacao.push(fim_tempo-inicio_tempo);
+                }
+            });
+
+            if (cor == cor_ref){
+                total_esperado++;
+            }
+            if (i<4) {
+                s = 3000;
+            }else if (i>3 && i<7){
+                s = 2500;
+            }else if (i>6 && i<12){
+                s = 2000;
+            }else if (i>11){
+                s = 1000;
+            }
+            $(id).css("background-color", "#f9fafb");
+//            $(".btn-get-restarted").on("click", function(){
+//                resetar();
+//                i=30;
+//                return false;
+//            });
+
+        }
+        mostrarResultado(acerto, total_esperado, tempo_reacao);
+
+    }
+    function resetar(){
+        $("#b18").css("background-color", "#f9fafb");
+        acerto = 0;
+        total_esperado = 0;
+        for (var i=1; i<17; i++){
+            var id = "#b"+i
+            $(id).css("background-color", "#f9fafb");
+        }
+    }
+    function mostrarResultado(acerto, total_esperado, tempo_reacao){
+        $(".btn-get-restarted").on("click", function(){
+            resetar();
+            $('#acertos').html("");
+            $('#erros').html("");
+        });
+        acertos = "Você fez " + acerto + " acertos";
+        erros = "Essa cor apareceu "+ (total_esperado)+ " vezes."+ " Você clicou errado "+ (total_esperado-acerto) + " vezes." + "Tempos de reação: "+tempo_reacao;
+        $('#acertos').html(acertos);
+        $('#erros').html(erros);
+
+    }
+
+    $(".btn-get-started").on("click", function(){
+        acendeBotoes();
+    });
+    $(".btn-get-restarted").on("click", function(){
+        resetar();
+    });
+
+});
+
